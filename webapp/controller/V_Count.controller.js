c90bfc457c20a6f783ea69158bd518f2d6238945
KDT <tangdunke@gmail.com>
2019-12-12 17:51:02 +0800

    Update on 20191212

--- webapp/controller/V_Count.controller.js
+++ webapp/controller/V_Count.controller.js
@@ -18,6 +18,7 @@ sap.ui.define([
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			// Validate/Match the Router Details sent from Input Controller
 			oRouter.getRoute("Target_V_Count").attachMatched(this._onRouteFound, this);
+
 		},
 
 		/**
@@ -54,21 +55,97 @@ sap.ui.define([
 			var lv_DT = oArgument.p_DT;
 			var lv_BP = oArgument.p_BP;
 			var lv_PT = oArgument.p_PT;
-			var datefilter = "?$filter=CreatedAt gt datetime'" + lv_DT + "T00:00:00'";
+
+			var datefilter = "?$filter=CreatedAt gt datetime'" + lv_DT + "T23:00:00.0000000'";
 			var businessPartnerfilter = "and CustomerID eq '" + lv_BP + "'";
-			var productfilter = "and ProductID eq '" + lv_PT + "'";
+			//var productfilter = "and ProductID eq '" + lv_PT + "'";
 
 			//var oModelM = new sap.ui.model.odata.ODataModel("/sap/opu/odata/sap/ZGW_PREQ_SRV/");
-			//var Entity = "MethodtextSet?$filter=(InspLot eq '890000000451' and Inspoper eq '0010' and Inspchar eq '0010' and Material eq 'NUGGET')";
-			//var Entity =  "PReqSet?$inlinecount=allpages";
-			var Entity = "SalesOrderSet" + datefilter + businessPartnerfilter; //+ productfilter;
-			var textData = [];
-			oModelM.read(Entity, null, null, false, function(oData, response) {
-				textData = oData.results;
-			});
-			var v_count = textData.length + " Purchase Requisitions Line Items which are not complete.";
+			var oModelM = this.getOwnerComponent().getModel();
+			//oModelM.setUseBatch(true);
 
-			this.getView().byId("Count").setText(v_count);
+			//var Entity = "/SalesOrderSet" + datefilter + businessPartnerfilter; //+ productfilter;
+			var lv_DT = lv_DT + "T23:00:00.0000000Z";
+			var oFilterGroup = new sap.ui.model.Filter({
+				and: true,
+				filters: [
+					new sap.ui.model.Filter({
+						path: "CreatedAt",
+						operator: sap.ui.model.FilterOperator.GT,
+						value1: lv_DT
+					}),
+					new sap.ui.model.Filter({
+						path: "CustomerID",
+						operator: sap.ui.model.FilterOperator.EQ,
+						value1: lv_BP
+					})
+				]
+			});
+			var aFilters = [oFilterGroup];
+
+			// // 多条件筛选
+			// var oFilter1 = new sap.ui.model.Filter(
+			//     "CreatedAt",
+			//     sap.ui.model.FilterOperator.GT,
+			//     lv_DT
+			// );
+
+			// var oFilter2 = new sap.ui.model.Filter(
+			//     "CustomerID",
+			//     sap.ui.model.FilterOperator.EQ,
+			//     lv_BP
+			// );
+
+			// // 实例化一个新的Filter, 参数2为false
+			// var oFilterGroup = new sap.ui.model.Filter([oFilter1, oFilter2], true);
+			// var aFilters = [oFilterGroup];
+
+			var oView = this.getView();
+			var textData = [];
+
+			function fnSuccess_in(oData, oResponse) {
+				sap.m.MessageToast.show("Successful");
+
+				textData = oData.results;
+
+				console.log(textData);
+				console.log(textData.length);
+
+				var v_count = textData.length + " Purchase Requisitions Line Items which are not complete.";
+				//sap.ui.getCore().byId("__xmlview2--Count").setText(v_count);
+				oView.byId("Count").setText(v_count);
+			}
+
+			function fnError_in(oError) {
+				sap.m.MessageToast.show("Failure");
+				console.log("Error", oError);
+			}
+			
+			// // 方式1--先读取内容，然后计算内容长度
+			// oModelM.read("/SalesOrderSet", {
+			// 	"async": false, //在v2版本的ODataModel中async参数无效
+			// 	// "urlParameters": {
+			// 	// 	"$format": "xml"
+			// 	// },
+			// 	"filters": aFilters,
+			// 	"success": fnSuccess_in,
+			// 	"error": fnError_in
+			// });
+			
+			// 方式2--直接读取OData长度
+			oModelM.read("/SalesOrderSet/$count", {
+				"async": false, //在v2版本的ODataModel中async参数无效
+				// "urlParameters": {
+				// 	"$format": "xml"
+				// },
+				"filters": aFilters,
+				"success": function(oData, oResponse) {
+					var v_count = oData + " Purchase Requisitions Line Items which are not complete.";
+					//sap.ui.getCore().byId("__xmlview2--Count").setText(v_count);
+					oView.byId("Count").setText(v_count);
+				 },
+				"error": fnError_in
+			});
 
 			var oJson = {};
 			oJson.vDT = lv_DT;
@@ -79,7 +156,7 @@ sap.ui.define([
 			// If we want this Model is another View/Page, we need to setModel		
 			//		sap.ui.getCore().setModel(this.myLocalModel);
 
-		},
+		}
 
 	});
 
